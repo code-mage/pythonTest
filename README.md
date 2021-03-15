8ae5330a6bed8c9085a2d301f1e15b68f95214c9
code-mage <lalaithhaleth@outlook.com>
2021-03-15 16:02:07 -0700

    Day 1

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# pythonTest
+# Day 1
+
+1. Setup environment
+2. Python 2 vs Python 3
+3. Program on command line - Hello World
+4. IDE
+5. Basics - Interpreter etc.
+6. Setup Git
+7. Print Hello World and run file
+8. Variable Basics
+9. Comments
---
+++ one.py
@@ -0,0 +1,25 @@
+#Printing Hello World
+
+print("Hello World")
+
+# don't make i a reserved word, like print
+i = 10
+
+# print = 78
+# print (print)
+
+
+print (i)
+
+#variable can be reassigned
+i = 20
+
+print (i)
+
+# In python, it can be reassigned to a diff type - int to string
+i = "Whatever"
+
+print (i)
+
+
+#Wanna learn more about print in detail - format, add, sep and end
