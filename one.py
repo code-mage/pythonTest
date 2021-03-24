1415a72a8776ac47f927837ce2088923c43c3ab2
ApoorvaSanthosh <apurvagreen@gmail.com>
2021-03-24 11:43:35 +0530

    changes1

--- one.py
+++ one.py
@@ -31,7 +31,31 @@ print ("strings")
 string1 = "To forget the dead"
 string2 = "would be akin to killing them"
 string3 = "a second time"
-
+#problem1
 print(string1, end=" ")
 print(string2, end=" ")
 print(string3 )
+#problem2
+
+#method1
+
+string4=string1+" "+string2+" "+ string3
+print(string4)
+
+#mehod 2
+
+print(string1,string2,string3)
+
+#problem 3
+
+string5=string1+"-"+string2+"-"+ string3 
+print(string5)
+
+#problem 4
+
+string1 = string1 + string2
+string2 = string1[0:(len(string1)-len(string2))]
+string1 = string1[len(string2):]
+
+print(string1)
+print(string2)
