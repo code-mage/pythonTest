6f2464809ebb85772d72c0e2876b89e0429d41bb
code-mage <lalaithhaleth@outlook.com>
2021-03-26 23:57:53 -0700

    TODO 1 - From f31d7ec320ae69a2ed32abd5e9e023f30f749e8c, 1415a72a8776ac47f927837ce2088923c43c3ab2

--- Todo/one.py
+++ Todo/one.py
@@ -5,28 +5,48 @@ string3 = "a second time"
 # Problem 1
 # Multiple print statements, that prints these strings in the same line.
 # Output expected - To forget the dead would be akin to killing them a second time
-print(string1)
-print(string2)
-print(string3)
+print(string1, end=" ")         #instead of ending the string with a new line, the end parameter ends the string with the value provided in it
+print(string2, end=" ")
+print(string3 )
 
 # Problem 2
 # A single print statement that prints all these three strings seperated by a space
 # Output expected - To forget the dead would be akin to killing them a second time
 # There are 2 ways to do this
-print()
+
+# Method1
+print(string1+" "+string2+" "+ string3)
+
+# Method2
+print(string1,string2,string3)
 
 # Problem 3
 # A single print statement that prints all these three strings seperated by a dash
 # Output expected - To forget the dead-would be akin to killing them-a second time
-print()
+
+# Method1
+print(string1+"-"+string2+"-"+ string3 )
+
+# Method2
+print(string1,string2,string3, sep="-")     # sep seperates the strings entered in the statement with the value inside sep.
 
 # Problem 4
 # Swap variables string1 and string2
 # There are two ways to do this
 
 
-
-
+# Method 1
+string1 = string1 + string2
+string2 = string1[0:(len(string1)-len(string2))]
+string1 = string1[len(string2):]
 
 print(string1)      # should read - would be akin to killing them
 print(string2)      # should read - To forget the dead
+
+#Method 2
+string4 = string1
+string1 = string2
+string2 = string4
+
+print(string1)      # should read - To forget the dead
+print(string2)      # should read - would be akin to killing them
