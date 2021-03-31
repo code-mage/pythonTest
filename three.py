14969d4c9383765e34d265142c4973dc4830cfd3
ApoorvaSanthosh <apurvagreen@gmail.com>
2021-03-31 21:57:17 +0530

    changes3-31

--- three.py
+++ three.py
@@ -1,14 +1,4 @@
-i = 15
-j = "15"
-k = 15.0
-l = True
-m = int(input())
-n= float(input())
-o = input()
-p = bool(input())
-q = str(k)
-print ("hello"+ q)
-print ("hello"+ k)
+
 #problem 1
 
 print("enter two values")
