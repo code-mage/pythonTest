7fc42550222614aef7a6b496c3a3c0ac256e59dc
Apoorva Santhosh <apurvagreen@gmail.com>
2021-04-13 21:53:27 +0530

    hwday4

---
+++ five.py
@@ -0,0 +1,26 @@
+# Print only odd values in an array
+
+x = input(list())
+print (x)
+
+for i in x:
+    y = int(i)
+    print (y)
+    if y % 2 != 0:
+        print (i)
+
+# find common values between two arrays
+x = input(list())
+y = input(list())
+
+print (x)
+print (y)
+
+for i in x:
+    if i in y:
+        print (i)
+# find max value in an array
+# Using 3, can you sort an array. O(n^2) will be revisited.
+# Given an array, can you find it it is sorted.
+# XOR Gate
+# Binary
