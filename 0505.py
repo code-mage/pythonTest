4603bd45792eefee8c5dff0255f318f23683754b
Apoorva Santhosh <apurvagreen@gmail.com>
2021-05-05 16:43:59 +0530

    0505

---
+++ 0505.py
@@ -0,0 +1,28 @@
+
+
+# for i in range (0,4):
+#     for j in range (0,10):
+#         print ("*", end = " ")
+#     print ()
+
+# for i in range (0,5):
+#     for j in range (0,i+1):
+#       print ("*", end = " ")
+#     print ()
+# n = 5
+# k = n - 1
+# for i in range (0,n):
+#     for j in range (0,k):
+#         print (end = " ")
+#     k=k-1
+#     for j in range (0,i):
+#         print ("*", end ="")
+#     print()
+
+# n = 3
+# k = 0
+# for i in range (1,n+1):
+#     k = k + 1/i
+
+# print (k)
+
